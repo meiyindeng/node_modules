0420b92a6420375e04407ef6cac5350fbc7d6cad
“Meiyin <meiyindeng@Meiyins-MacBook-Pro.local>
2020-11-18 00:21:16 -0800

    callback and error handling

---
+++ index.js
@@ -0,0 +1,27 @@
+var rect = require('./rectangle');
+    
+ function solveRect(l, b) {
+     console.log("Solving for rectangle with l = " + l + " and b = " + b);
+    
+     rect(l, b, (err, rectObject) => {
+         if(err) {
+             console.log("ERROR: ", err.message);
+         }
+         else {
+             console.log( " The area of the rectangle of dimensions l = " + l + " and b = " + b 
+             + " is " + rectObject.area());
+             
+             console.log( " The perimeter of the rectangle of dimensions l = " + l + " and b = " + b
+             + " is " + rectObject.perimeter());
+             
+         }
+
+        });    
+        console.log("This is after the call to rect()");
+     
+ };
+
+ solveRect(2, 4);
+ solveRect(3, 5);
+ solveRect(0, 5);
+ 
